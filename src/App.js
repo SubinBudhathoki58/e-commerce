4d33d0f29dbb807bf2476d7cd5869c6e4a0149c2
Subin Budhathoki <subinbudhathoki09@gmail.com>
2024-04-17 08:36:56 +0545

    setting up

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./Components/Navbar";
+import ProductList from "./Components/ProductList";
+import Cart from "./Components/Cart";
+import Default from "./Components/Default";
+import Details from "./Components/Details";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    <React.Fragment>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/productlist" Component={ProductList} />
+          <Route exact path="/details" Component={Details} />
+          <Route exact path="/cart" Component={Cart} />
+          <Route exact path="/default" Component={Default} />
+        </Routes>
+      </Router>
+    </React.Fragment>
+  );
+}
+
+export default App;
